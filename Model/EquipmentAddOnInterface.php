ef2b5f1cf56d62f82ce4437ed698bbdd4525a072
owl-app <owl@pawelkeska.eu>
2023-06-23 15:05:51 +0000

    Fixes

--- Model/EquipmentAddOnInterface.php
+++ Model/EquipmentAddOnInterface.php
@@ -7,5 +7,11 @@ namespace Owl\Component\Core\Model;
 use Owl\Component\Category\Model\CategoryInterface as BaseCategoryInterface;
 
 interface EquipmentAddOnInterface {
+    public const ADDON_REFUELING_NAME = 'owl.ui.equipment_addon.refueling_name';
 
+    public const ADDON_EVENT_NAME = 'owl.ui.equipment_addon.event_name';
+
+    public const ADDON_REFUELING_CODE = 'refueling';
+
+    public const ADDON_EVENT_CODE = 'event';
 }
--- Model/EquipmentCategory.php
+++ Model/EquipmentCategory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Owl\Component\Core\Model;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Owl\Component\Category\Model\Category as BaseCategory;
 
 class EquipmentCategory extends BaseCategory implements EquipmentCategoryInterface
