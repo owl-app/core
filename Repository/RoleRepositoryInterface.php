ce2d9f170358d046310241e81b8ac5b6b2fb4480
owl-app <owl@pawelkeska.eu>
2023-09-12 09:06:39 +0000

    Psalm fixes

--- Repository/RoleRepositoryInterface.php
+++ Repository/RoleRepositoryInterface.php
@@ -5,15 +5,16 @@ declare(strict_types=1);
 namespace Owl\Component\Core\Repository;
 
 use Doctrine\ORM\QueryBuilder;
+use Owl\Component\Core\Model\Rbac\RoleInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Owl\Component\Equipment\Model\EquipmentAttributeInterface;
 
 /**
- * @template T of EquipmentAttributeInterface
+ * @template T of RoleInterface
  *
  * @extends RepositoryInterface<T>
  */
-interface EquipmentAttributeRepositoryInterface extends RepositoryInterface
+interface RoleRepositoryInterface extends RepositoryInterface
 {
-    public function findByCategory($categoryId): QueryBuilder;
+    public function findWithoutAdminSystem(): array;
 }
